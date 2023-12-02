510dc5573d45f36dfeb7a025ad34a986260c26dd
VladaGromova <vladagromova1106@gmail.com>
2023-12-02 12:31:07 +0100

    parallel matrix fill print

--- parallel.cu
+++ parallel.cu
@@ -112,6 +112,33 @@ void InitializeMatrix(Matrix& mat, int width, int height, int realWidth, int rea
   }
 }
 
+__global__ void MatMulKernel(Matrix A, Matrix B, Matrix C, unsigned long long* time, int BLOCK_SIZE) {
+    
+    int blockRow = blockIdx.y;
+    int blockCol = blockIdx.x;
+    Matrix Csub = GetSubMatrix(C, blockRow, blockCol);
+    float Cvalue = 0;
+    int row = threadIdx.y;
+    int col = threadIdx.x;
+
+    unsigned long long startTime = clock();
+   for (int m = 0; m < (A.width / BLOCK_SIZE); ++m) {
+        Matrix Asub = GetSubMatrix(A, blockRow, m);
+        Matrix Bsub = GetSubMatrix(B, m, blockCol);
+        __shared__ float As[BLOCK_SIZE][BLOCK_SIZE];
+        __shared__ float Bs[BLOCK_SIZE][BLOCK_SIZE];
+        As[row][col] = GetElement(Asub, row, col);
+        Bs[row][col] = GetElement(Bsub, row, col);
+        __syncthreads();
+        for (int e = 0; e < BLOCK_SIZE; ++e)
+            Cvalue += As[row][e] * Bs[e][col];
+        __syncthreads();
+    }
+    SetElement(Csub, row, col, Cvalue);
+    unsigned long long finishTime = clock();
+    *time = (finishTime - startTime);
+}
+
 int main() {
 
   std::ifstream inputFile(FILENAME);
@@ -172,46 +199,52 @@ int main() {
     std::cout << std::endl;
   }
 
-  // Matrix d_A, d_B, d_C;
-  // d_A.width = d_A.stride = A.width; 
-  // d_A.height = A.height;
-  // d_B.width = d_B.stride = B.width; 
-  // d_B.height = B.height;
-  // d_C.width = d_C.stride = C.width; 
-  // d_C.height = C.height;
-  // cudaMalloc(&d_A.elements, A.width * A.height * sizeof(float));
-  // cudaMemcpy(d_A.elements, A.elements, A.width * A.height * sizeof(float),
-  //            cudaMemcpyHostToDevice);
+  Matrix d_A, d_B, d_C;
+  d_A.width = d_A.stride = A.width; 
+  d_A.height = A.height;
+  d_A.realWidth = A.realWidth;
+  d_A.realHeight = d_A.realHeight;
+  d_B.width = d_B.stride = B.width; 
+  d_B.height = B.height;
+  d_B.realWidth = B.realWidth;
+  d_B.realHeight = B.realHeight;
+  d_C.width = d_C.stride = C.width; 
+  d_C.height = C.height;
+  d_C.realWidth = C.realWidth;
+  d_C.realHeight = C.realHeight;
+  cudaMalloc(&d_A.elements, A.width * A.height * sizeof(float));
+  cudaMemcpy(d_A.elements, A.elements, A.width * A.height * sizeof(float),
+             cudaMemcpyHostToDevice);
 
-  // cudaMalloc(&d_B.elements, B.width * B.height * sizeof(float));
-  // cudaMemcpy(d_B.elements, B.elements, B.width * B.height * sizeof(float),
-  //            cudaMemcpyHostToDevice);
+  cudaMalloc(&d_B.elements, B.width * B.height * sizeof(float));
+  cudaMemcpy(d_B.elements, B.elements, B.width * B.height * sizeof(float),
+             cudaMemcpyHostToDevice);
 
-  // cudaMalloc(&d_C.elements, C.width * C.height * sizeof(float));
+  cudaMalloc(&d_C.elements, C.width * C.height * sizeof(float));
 
-  // dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE); 
-  // dim3 dimGrid((int) ceil((double)B.width / (double)dimBlock.x),
-  //              (int) ceil((double)A.height /(double) dimBlock.y)); 
+  dim3 dimBlock(block_size, block_size); 
+  dim3 dimGrid((int) ceil((double)B.width / (double)dimBlock.x),
+               (int) ceil((double)A.height /(double) dimBlock.y)); 
 
-  // unsigned long long time;
-  // unsigned long long* d_time;
-  // cudaMalloc(&d_time, sizeof(unsigned long long));
+  unsigned long long time;
+  unsigned long long* d_time;
+  cudaMalloc(&d_time, sizeof(unsigned long long));
 
-  // MatMulKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, d_time); 
-  // cudaMemcpy(&time, d_time, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
-  // std::cout<<"Time: "<<time<<'\n';
-  // cudaMemcpy(C.elements, d_C.elements, C.width * C.height * sizeof(float),
-  //            cudaMemcpyDeviceToHost);
-  // std::cout << "Matrix C:" << std::endl;
-  // for (int i = 0; i < C.height; ++i) {
-  //   for (int j = 0; j < C.width; ++j) {
-  //     std::cout << GetElementCPU(C, i, j) << " ";
-  //   }
-  //   std::cout << std::endl;
-  // }
-  // cudaFree(d_A.elements);
-  // cudaFree(d_B.elements);
-  // cudaFree(d_C.elements);
-  // cudaFree(d_time);
+  MatMulKernel<<<dimGrid, dimBlock>>>(d_A, d_B, d_C, d_time, block_size); 
+  cudaMemcpy(&time, d_time, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
+  std::cout<<"Time: "<<time<<'\n';
+  cudaMemcpy(C.elements, d_C.elements, C.width * C.height * sizeof(float),
+             cudaMemcpyDeviceToHost);
+  std::cout << "Matrix C:" << std::endl;
+  for (int i = 0; i < C.height; ++i) {
+    for (int j = 0; j < C.width; ++j) {
+      std::cout << GetElementCPU(C, i, j) << " ";
+    }
+    std::cout << std::endl;
+  }
+  cudaFree(d_A.elements);
+  cudaFree(d_B.elements);
+  cudaFree(d_C.elements);
+  cudaFree(d_time);
   return 0;
 }
