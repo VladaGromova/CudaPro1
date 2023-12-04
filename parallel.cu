de6bd38c8502a972879a7424503ebee21aa40a92
VladaGromova <vladagromova1106@gmail.com>
2023-12-05 00:55:38 +0100

    updates

--- parallel.cu
+++ parallel.cu
@@ -12,12 +12,12 @@
 
 #pragma hd_warning_disable
 //#define FILENAME "points_generated.txt"
-//#define FILENAME "data.txt"
-#define FILENAME "myData.txt"
+#define FILENAME "data.txt"
+//#define FILENAME "myData.txt"
 //#define FILENAME "cluster_data.txt"
 
 #define BLOCK_SIZE 16
-#define MAX_ITERATIONS 100
+#define MAX_ITERATIONS 1
 #define EPS 0.000001f
 //#define MAX_THREADS_IN_BLOCK 512
 
