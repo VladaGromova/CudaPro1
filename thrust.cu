6e89025dcf7b0c8f40de413237d6efa2672f57d7
VladaGromova <vladagromova1106@gmail.com>
2023-12-06 13:51:31 +0100

    updates

--- thrust.cu
+++ thrust.cu
@@ -154,13 +154,16 @@ struct NotEqual {
   }
 };
 
-unsigned long long eucl_dist_thrust(thrust::host_vector<float> &cs,
+unsigned long long eucl_dist_thrust(/*thrust::host_vector<float> &cs,
                                     thrust::host_vector<float> &data,
-                                    thrust::host_vector<float> &dist, int k,
-                                    int n, int N, int print) {
+                                    thrust::host_vector<float> &dist,*/
+                                    float *&data, float *&cs, float *&clstrs,
+                                    int k, int n, int N, int print) {
 
-  thrust::device_vector<float> d_data = data;
-  thrust::device_vector<float> d_centr = cs;
+  // thrust::device_vector<float> d_data = data;
+  // thrust::device_vector<float> d_centr = cs;
+  thrust::device_vector<float> d_data(data, data + n*N);
+  thrust::device_vector<float> d_centr(cs, cs + n*k);
   thrust::device_vector<float> values_out(k * N);
 
   int delta = INT_MAX;
@@ -289,13 +292,15 @@ unsigned long long eucl_dist_thrust(thrust::host_vector<float> &cs,
     }
     ++numIters;
   }
+   clstrs = new int[old_d_clusters.size()];
+    thrust::copy(old_d_clusters.begin(), old_d_clusters.end(), clstrs);
 
   compute_time = dtime_usec(compute_time);
   std::cout << "Iterations:" << numIters << '\n';
-  std::cout << "\n Centroids:\n";
-  thrust::copy_n(d_centr.begin(), d_centr.end(),
-                 std::ostream_iterator<float>(std::cout, ", "));
-  std::cout << std::endl;
+  // std::cout << "\n Centroids:\n";
+  // thrust::copy_n(d_centr.begin(), d_centr.end(),
+  //                std::ostream_iterator<float>(std::cout, ", "));
+  // std::cout << std::endl;
 
   return compute_time;
 }
@@ -325,6 +330,7 @@ int main(int argc, char **argv) {
 
   float *data = new float[N * n];
   float *centroids = new float[k * n];
+  int *clusters;
 
   float value = 0.0f;
   int ind = 0;
@@ -341,11 +347,19 @@ int main(int argc, char **argv) {
   }
   inputFile.close();
 
-  thrust::host_vector<float> h_data(data, data + N * n);
-  thrust::host_vector<float> h_centr(centroids, centroids + k * n);
-  thrust::host_vector<float> h_dist(k * N);
-  eucl_dist_thrust(h_centr, h_data, h_dist, k, n, N, 1);
+  // thrust::host_vector<float> h_data(data, data + N * n);
+  // thrust::host_vector<float> h_centr(centroids, centroids + k * n);
+  // thrust::host_vector<float> h_dist(k * N);
+  //eucl_dist_thrust(h_centr, h_data, h_dist, k, n, N, 1);
+  eucl_dist_thrust(data, centroids, clusters, k, n, N, 1);
 
-  std::cout << "\nBye!\n";
+  std::cout<<"Data:\n";
+  for(int i=0; i<N; ++i){
+    for(int j=0; j<n; ++j){
+      std::cout<<data[i*n + j]<<' ';
+    }
+    std::cout<<clusters[i]<<'\n';
+  }
+
   return 0;
 }
